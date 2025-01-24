cb0d7ea8efa90349d905ab85332057b361d417f0
Fredrik Jureen <fredrik.jureen@gmail.com>
2025-01-24 16:00:46 +0100

    [lib] Vec2d, make constructor constexpr, add xytoa()

--- source/lib/vec2d.cpp
+++ source/lib/vec2d.cpp
@@ -18,3 +18,9 @@ bool aoc24::Vec2d_comparator::operator()(const Vec2d& lhs,
 	}
 	return lhs.y < rhs.y;
 }
+
+std::string aoc24::xytoa(int pos_x, int pos_y)
+{
+	return std::string("(" + std::to_string(pos_x) + ", "
+	                   + std::to_string(pos_y) + ")");
+}
--- source/lib/vec2d.h
+++ source/lib/vec2d.h
@@ -1,11 +1,16 @@
 #ifndef VEC2D_H
 #define VEC2D_H
+#include <string>
 
 namespace aoc24 {
 
 struct Vec2d
 {
-	Vec2d(int xx, int yy)
+	constexpr Vec2d()
+	    : x(0)
+	    , y(0)
+	{}
+	constexpr Vec2d(int xx, int yy)
 	    : x(xx)
 	    , y(yy)
 	{}
@@ -19,6 +24,8 @@ struct Vec2d_comparator
 {
 	bool operator()(const aoc24::Vec2d& lhs, const aoc24::Vec2d& rhs) const;
 };
+
+std::string xytoa(int pos_x, int pos_y);
 
 } // namespace aoc24
 //------------------------------------------------------------------------------
