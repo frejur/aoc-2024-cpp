30307361ee993ee630b619fc75fb944a7e1a44d2
Fredrik Jureen <fredrik.jureen@gmail.com>
2025-02-03 23:51:13 +0100

    [day_15] Remove source and header files for the old solution

--- source/day_15/day_15.cpp
+++ source/day_15/day_15.cpp
@@ -12,6 +12,7 @@
 namespace {
 constexpr bool enable_debug{false};
 constexpr bool use_smaller_test_sample{false};
+
 const std::string& debug_output_filename_p1()
 {
 	static const std::string p{"aoc24_15_p_1_debug.txt"};
@@ -20,21 +21,6 @@ const std::string& debug_output_filename_p1()
 const std::string& debug_output_filename_p2()
 {
 	static const std::string p{"aoc24_15_p_2_debug.txt"};
-	return p;
-}
-const std::string& debug_output_filename_p1_new()
-{
-	static const std::string p{"aoc24_15_p_1_debug_new.txt"};
-	return p;
-}
-const std::string& debug_output_filename_p2_new()
-{
-	static const std::string p{"aoc24_15_p_2_debug_new.txt"};
-	return p;
-}
-const std::string& debug_output_filename_compare()
-{
-	static const std::string p{"aoc24_15_p_2_debug_compare.txt"};
 	return p;
 }
 } // namespace
@@ -75,66 +61,49 @@ try {
 	                              num_moves);
 
 	Char_grid char_grid(chars, sz);
-
-	// Part one
-	Box_grid_single grid_p1{char_grid, std::move(bitset_p1)};
-
-	long long p1_coord_sum{
-	    move_robot_and_get_sum_of_coordinates(grid_p1,
-	                                          moves,
-	                                          enable_debug,
-	                                          debug_output_filename_p1())};
-	// pz.file_answer(1, "Sum of coordinates", p1_coord_sum);
-
-	Box_grid_wide grid_p2{char_grid, std::move(bitset_p2)};
-	long long p2_coord_sum{
-	    move_robot_and_get_sum_of_coordinates(grid_p2,
-	                                          moves,
-	                                          enable_debug,
-	                                          debug_output_filename_p2())};
-	// pz.file_answer(2, "Sum of scaled up warehouse", p2_coord_sum);
-	// pz.print_answers();
-
-	// Refactor
-	Char_grid char_grid_refactor(chars, sz);
 	Vec2d spawn_xy;
 	std::vector<Vec2d> box_xy_v;
-	extract_spawn_and_box_positions(char_grid_refactor, spawn_xy, box_xy_v);
+	extract_spawn_and_box_positions(char_grid, spawn_xy, box_xy_v);
 
-	auto bitset_p1_refactor
-	    = (pz.is_testing()) ? aoc24::Bitset_factory::create<sz_test * sz_test>()
-	                        : aoc24::Bitset_factory::create<sz_inp * sz_inp>();
-
+	// Part one
 	Simple_box_factory sf;
-	Warehouse p1_wh{sz, sz, std::move(bitset_p1_refactor), char_grid_refactor};
+	Warehouse p1_wh{sz, sz, std::move(bitset_p1), char_grid};
 
 	for (const auto& xy : box_xy_v) {
 		p1_wh.add_box(sf, xy.x, xy.y);
 	}
 
 	p1_wh.spawn_robot(spawn_xy.x, spawn_xy.y);
-	long long p1_sum{move_robot_and_get_sum_of_coordinates_new(
-	    p1_wh, moves, enable_debug, debug_output_filename_p1_new())};
+
+	long long p1_sum{
+	    move_robot_and_get_sum_of_coordinates(p1_wh,
+	                                          moves,
+	                                          enable_debug,
+	                                          debug_output_filename_p1())};
 
 	pz.file_answer(1, "Sum of coordinates", p1_sum);
 
-	Char_grid char_grid_refactor_wide{new_wider_grid(char_grid_refactor)};
+	// Part two
+	Char_grid char_grid_wide{create_new_wider_grid(char_grid)};
 
 	auto bitset_p2_refactor
 	    = (pz.is_testing())
 	          ? aoc24::Bitset_factory::create<sz_test * sz_test * 2>()
 	          : aoc24::Bitset_factory::create<sz_inp * sz_inp * 2>();
 	Rect_box_factory rf;
-	Warehouse p2_wh{sz * 2,
-	                sz,
-	                std::move(bitset_p2_refactor),
-	                char_grid_refactor_wide};
+	Warehouse p2_wh{sz * 2, sz, std::move(bitset_p2_refactor), char_grid_wide};
+
 	for (const auto& xy : box_xy_v) {
 		p2_wh.add_box(rf, xy.x * 2, xy.y);
 	}
+
 	p2_wh.spawn_robot(spawn_xy.x * 2, spawn_xy.y);
-	long long p2_sum{move_robot_and_get_sum_of_coordinates_new(
-	    p2_wh, moves, enable_debug, debug_output_filename_p2_new())};
+
+	long long p2_sum{
+	    move_robot_and_get_sum_of_coordinates(p2_wh,
+	                                          moves,
+	                                          enable_debug,
+	                                          debug_output_filename_p2())};
 
 	pz.file_answer(2, "Sum of wide Warehouse", p2_sum);
 
@@ -152,45 +121,6 @@ try {
 //------------------------------------------------------------------------------
 
 long long aoc24_15::move_robot_and_get_sum_of_coordinates(
-    Box_grid& grid,
-    std::queue<aoc24::Direction> moves,
-    bool enable_debug,
-    const std::string debug_output_filename)
-{
-	std::unique_ptr<std::ofstream> ofs;
-	if (enable_debug) {
-		ofs.reset(new std::ofstream(debug_output_filename));
-		if (!(*ofs)) {
-			throw std::ios_base::failure("Could not open sample input file: ");
-		}
-		(*ofs) << "Initial state:" << '\n';
-		grid.print_map(*ofs);
-	}
-
-	const int total_num_moves = static_cast<int>(moves.size());
-	while (!moves.empty()) {
-		auto& m = moves.front();
-		grid.move_robot(m);
-		moves.pop();
-
-		if (enable_debug) {
-			if (total_num_moves > 1000 && grid.number_of_moves() % 1000 != 0) {
-				continue;
-			}
-
-			(*ofs) << "Move #" << grid.number_of_moves() << ": " << m << '\n';
-			grid.print_map(*ofs);
-
-			if (!grid.boxes_match_maps()) {
-				(*ofs) << "Error: The Box objects do not match the maps";
-				return -1;
-			}
-		}
-	}
-	return grid.sum_of_all_box_coordinates();
-}
-
-long long aoc24_15::move_robot_and_get_sum_of_coordinates_new(
     Warehouse& grid,
     std::queue<aoc24::Direction> moves,
     bool enable_debug,
@@ -282,7 +212,8 @@ std::ostream& aoc24_15::operator<<(std::ostream& ostr, aoc24::Vec2d& vec)
 	return ostr << '(' << vec.x << ", " << vec.y << ')';
 }
 
-aoc24::Char_grid aoc24_15::new_wider_grid(const aoc24::Char_grid& reference_grid)
+aoc24::Char_grid aoc24_15::create_new_wider_grid(
+    const aoc24::Char_grid& reference_grid)
 {
 	aoc24::Char_grid new_grid{'.',
 	                          reference_grid.width() * 2,
--- source/day_15/day_15.h
+++ source/day_15/day_15.h
@@ -1,18 +1,10 @@
 #ifndef DAY_15_H
 #define DAY_15_H
-#include "box_grid_single.h"
-#include "box_grid_wide.h"
 #include "warehouse.h"
 #include <queue>
 
 namespace aoc24_15 {
 long long move_robot_and_get_sum_of_coordinates(
-    Box_grid& grid,
-    std::queue<aoc24::Direction> moves,
-    bool enable_debug = false,
-    const std::string debug_output_filename = "");
-
-long long move_robot_and_get_sum_of_coordinates_new(
     Warehouse& grid,
     std::queue<aoc24::Direction> moves,
     bool enable_debug = false,
@@ -27,7 +19,8 @@ void read_grid_and_moves_from_file(const std::string& file_path,
 void extract_spawn_and_box_positions(aoc24::Char_grid& grid,
                                      aoc24::Vec2d& spawn_pos,
                                      std::vector<aoc24::Vec2d>& box_pos_v);
-aoc24::Char_grid new_wider_grid(const aoc24::Char_grid& grid);
+
+aoc24::Char_grid create_new_wider_grid(const aoc24::Char_grid& grid);
 
 std::ostream& operator<<(std::ostream& ostr, aoc24::Vec2d& vec);
 
